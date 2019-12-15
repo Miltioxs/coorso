d11c6801b2c9b1426ca61760bb94c3c6ac2a2e7e
EdwinAlmira <ealopezalmira@gmail.com>
2019-12-15 09:33:13 -0600

    Uniendo vista de oferta especifica

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 route::resource('/miscursos' , 'miscursosController');
-=======
-route::resource('/mis_cursos' , 'miscursosController');
 route::resource('/misaplicaciones' , 'aplicacionesController');
 route::resource('/requerimientos' , 'requerimientosController');
->>>>>>> 969a8b5e21a336cfb2c0898f13d8717f62ade8a7
 route::resource('/empresa_', 'empresaController');
 route::resource('/cursos', 'cursoController');
 route::resource('/clases', 'clasesController');
